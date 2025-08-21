4c7e9366571d9bffb1c4e411c0c022a7e2849ac4
fahmi12musyaffa <muhammad_musyaffa_34rpl@student.smktelkom-mlg.sch.id>
2025-08-21 07:52:26 +0700

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,22 +1,31 @@
-public class SavingsAccount {
-    public double balance ;
-    public double interestRate = 0.01;
+public class SavingsAccount2 {
+    public double balance;
     public String name;
 
-   
-    public void deposit(int x){
-       balance += x;
-
-    }//end method deposit
-
-    public double calcInterest(){
-        return balance * interestRate;
-    }
-    public void displayCustomer (){
+  
+    public void displayCustomer() {
         System.out.println("Customer Name: " + name);
         System.out.println("Balance: " + balance);
-        System.out.println("Interest Rate: " + interestRate);
         System.out.println();
-    }//end method displayCustomer 
-}//end class SavingsAccount
 
+    }public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited: " + amount);
+        System.out.println("New Balance: " + balance);
+        System.out.println();
+
+    }
+    public void withdraw(double amount) {
+        if (amount < 0) {
+            System.out.println("Cannot withdraw a negative amount.");
+        } else if (amount <= balance) {
+            System.out.println("Cannot withdraw more than the current balance.");
+        } else {}
+            balance -= amount;
+            System.out.println("Withdrawn: " + amount);
+            System.out.println("New Balance: " + balance);
+    }
+}
+    
+          
+ 
