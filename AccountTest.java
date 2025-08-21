c89dc7591a5f621872e4cc947ab644e924905fd2
fahmi12musyaffa <muhammad_musyaffa_34rpl@student.smktelkom-mlg.sch.id>
2025-08-21 08:04:25 +0700

    Update AccountTest.java

--- AccountTest.java
+++ AccountTest.java
@@ -4,14 +4,15 @@ public class AccountTest2 {
         SavingsAccount2 sa0001 = new SavingsAccount2();
         sa0001.name = "Fahmi";
         sa0001.balance = 5000;
-
         sa0001.displayCustomer();
         sa0001.deposit(1000);
+        sa0001.balance += sa0001.calcInterest();
         sa0001.balance += 0.5;
         sa0001.withdraw(200);
         sa0001.displayCustomer();
     }
 }
+
 
 
 
